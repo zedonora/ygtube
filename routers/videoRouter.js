e22476c2613bca59e8fa2d07d6965c221d349abe
zedonora <timeundtime@gmail.com>
2019-01-04 05:42:14 +0900

    11th

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,10 @@
 import express from "express";
-
+import routes from "../routers";
 const videoRouter = express.Router();
+videoRouter.get(routes.videos, (req, res) => res.send("Vidoes"));
+videoRouter.get(routes.upload, (req, res) => res.send("Upload"));
+videoRouter.get(routes.videoDetail, (req, res) => res.send("Video Detail"));
+videoRouter.get(routes.editVideo, (req, res) => res.send("Edit Video"));
+videoRouter.get(routes.deleteVideo, (req, res) => res.send("Delete Video"));
 
 export default videoRouter;
