54f72635625a0842a5d5d979ead164f2671c34af
zedonora <timeundtime@gmail.com>
2019-01-17 05:23:33 +0900

    passport

--- init.js
+++ init.js
@@ -1,9 +1,11 @@
+import dotenv from "dotenv";
 import "./db";
 import app from "./app";
-import dotenv from "dotenv";
+
 dotenv.config();
 import "./models/Video";
 import "./models/Comment";
+import "./models/User";
 const PORT = process.env.PORT || 4000;
 const handleListening = () =>
   console.log(`✅  Listening on: http://localhost:${PORT}`);
